bdbdcaa12a6eb3ea46d8a77d40422dcdd612d786
Marko <galicmmarko@gmail.com>
2020-09-23 17:05:56 +0200

    App is in progress...

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const { response } = require('express');
+const { render } = require('ejs');
+const app = express();
+const blogRouters = require('./routes/blogRoutes');
+
+const dbURI = 'mongodb+srv://marko:test1234@Testing.hqdlt.mongodb.net/Testing?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err))
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title : 'About' });
+});
+
+app.use(blogRouters);
+
+
+app.use((req, res) => {
+    res.status(404).render('404', { title : '404' });
+});
